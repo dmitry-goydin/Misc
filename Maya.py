9263ccb792977f4dbb346147cb17c2c5ecc9d940
Dmitry B. Goydin <dmitry.goydin@gmail.com>
2019-09-17 08:30:11 +0800

    Added new method to return found result as an array.

--- Maya.py
+++ Maya.py
@@ -7,3 +7,12 @@ def divisible_palindrome(max, divisor):
             break
         num -= 1
     return result
+
+def divisible_palindromes(max, divisor):
+    result = []
+    num = max - 1
+    while num > 10:
+        if num % divisor == 0 and str(num) == str(num)[::-1]:
+            result.append(num)
+        num -= 1
+    return result
--- test_divisible_polindrome.py
+++ test_divisible_polindrome.py
@@ -4,4 +4,13 @@ from unittest import TestCase
 class TestDivisiblePalindrome(TestCase):
     def test_divisible_palindrome(self):
         from Maya import divisible_palindrome
-        self.assertEqual(divisible_palindrome(150, 11), 121, "Expected 121")
+        self.assertEqual(divisible_palindrome(150, 11), 121, "Unexpected result for (150, 11)")
+
+
+class TestDivisiblePalindromes(TestCase):
+    def test_divisible_palindromes(self):
+        from Maya import divisible_palindromes
+        self.assertEqual(divisible_palindromes(150, 11), [121, 99, 88, 77, 66, 55, 44, 33, 22, 11],
+                         "Unexpected result for (150, 11)")
+        self.assertEqual(divisible_palindromes(150, 7), [77], "Unexpected result for 150, 7")
+        self.assertEqual(divisible_palindromes(1000, 17), [969, 646, 595, 323, 272], "Unexpected result for (1000, 17)")
