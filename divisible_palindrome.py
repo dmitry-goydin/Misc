3674b2a9db4cb273fae883b67742b517c2d2a4f9
Dmitry B. Goydin <dmitry.goydin@gmail.com>
2019-10-27 00:53:07 +0800

    Added some comments.

--- divisible_palindrome.py
+++ divisible_palindrome.py
@@ -1,4 +1,10 @@
 def divisible_palindromes(max, divisor):
+    """
+    Function returns palindrome numbers within given range that are also divisible by provided number.
+    :param max: upper limit of the range
+    :param divisor: the number the returned palindrome numbers have to be divisible by
+    :return: list of palindrome numbers
+    """
     result = []
     if divisor > 0 and max >= divisor and max > 10:
         num = divisor
